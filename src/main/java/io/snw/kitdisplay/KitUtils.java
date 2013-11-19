6f0a6e92a073b5c7171204633f59ff41b6a58595
hawkfalcon <hawkfalcon1@gmail.com>
2013-11-18 23:46:37 -0800

    Split stacks >64 items

--- src/main/java/io/snw/kitdisplay/KitUtils.java
+++ src/main/java/io/snw/kitdisplay/KitUtils.java
@@ -7,7 +7,6 @@ import com.earth2me.essentials.textreader.IText;
 import com.earth2me.essentials.textreader.KeywordReplacer;
 import com.earth2me.essentials.textreader.SimpleTextInput;
 import net.ess3.api.IEssentials;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -21,6 +20,7 @@ public class KitUtils {
         this.plugin = plugin;
     }
 
+    //from essentials
     public List<ItemStack> getExpandedItems(final IEssentials ess, final List<String> items) throws Exception {
         List<ItemStack> itemList = new ArrayList<ItemStack>();
         IText input = new SimpleTextInput(items);
@@ -37,8 +37,38 @@ public class KitUtils {
             if (parts.length > 2) {
                 metaStack.parseStringMeta(null, allowUnsafe, parts, 2, ess);
             }
-            itemList.add(metaStack.getItemStack());
+            ItemStack is = metaStack.getItemStack();
+            if (is.getAmount() > 64) {
+               for (ItemStack stack : splitIntoStacks(is, is.getAmount())) {
+                    itemList.add(stack);
+                }
+            } else {
+            itemList.add(is);
+            }
         }
         return itemList;
     }
+
+    //https://forums.bukkit.org/threads/itemstack-splitter.86892/
+    private ItemStack[] splitIntoStacks(ItemStack item, int amount) {
+
+        final int maxSize = item.getMaxStackSize();
+        final int remainder = amount % maxSize;
+        final int fullStacks = (int) Math.floor(amount / item.getMaxStackSize());
+
+        ItemStack fullStack = item.clone();
+        ItemStack finalStack = item.clone();
+        fullStack.setAmount(maxSize);
+        finalStack.setAmount(remainder);
+
+        ItemStack[] items = new ItemStack[fullStacks + 1];
+
+        for (int i = 0; i < fullStacks; i++) {
+            items[i] = fullStack;
+        }
+        items[items.length - 1] = finalStack;
+
+        return items;
+    }
+
 }
